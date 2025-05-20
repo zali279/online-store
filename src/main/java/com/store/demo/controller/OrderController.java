275812ae30c81193dc14216338667476bc18d3f9
zali279 <zahraaalihasan279@gmail.com>
2025-05-20 14:33:58 +0300

    implement get order receipt end point

--- src/main/java/com/store/demo/controller/OrderController.java
+++ src/main/java/com/store/demo/controller/OrderController.java
@@ -1,20 +1,56 @@
 package com.store.demo.controller;
 
+import com.store.demo.model.Order;
+import com.store.demo.repository.OrderRepository;
 import com.store.demo.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/order")
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderService orderService;
+    private final OrderRepository orderRepository;
 
     @GetMapping("/history")
     public ResponseEntity<?> orderHistory(){
         return ResponseEntity.ok(orderService.getOrderHistory());
     }
+
+    @GetMapping("/{orderId}/receipt")
+    public ResponseEntity<Resource> getReceiptFile(@PathVariable Long orderId) throws IOException {
+        Optional<Order> optionalOrder = orderRepository.findById(orderId);
+
+        if (optionalOrder.isEmpty() || optionalOrder.get().getReceiptPath() == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Path path = Paths.get(optionalOrder.get().getReceiptPath());
+        if (!Files.exists(path)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_PDF)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + path.getFileName() + "\"")
+                .body(resource);
+    }
+
 }
