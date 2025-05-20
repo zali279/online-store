76ea9e3bc5c3c15bbdda126f60345a88d9f77415
zali279 <zahraaalihasan279@gmail.com>
2025-05-20 14:36:32 +0300

    replace application.properties with application.yml

--- src/main/java/com/store/demo/model/User.java
+++ src/main/java/com/store/demo/model/User.java
@@ -2,11 +2,15 @@ package com.store.demo.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Data
 @RequiredArgsConstructor
+@ToString(exclude = {"cart"})  // avoid recursion
+@EqualsAndHashCode(exclude = {"cart"}) // avoid recursion
 @Table(name = "users")
 public class User {
     @Id
